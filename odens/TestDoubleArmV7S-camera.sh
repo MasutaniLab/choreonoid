8f299d70798f54c790ff5de9159782ad8e93b22f
MASUTANI Yasuhiro <masutani@osakac.ac.jp>
2018-08-19 21:33:24 +0900

    2つのやり方で外部RTCと接続する試行

--- odens/TestDoubleArmV7S-camera.sh
+++ odens/TestDoubleArmV7S-camera.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-(cd ..; bin/choreonoid sample/OpenRTM/OpenRTM-DoubleArmV7S-camera.cnoid &)
-./TkSlider.py &
-./TkMonitorSlider.py &
-#../../ImageViewerがあることが前提
-../../ImageViewer/ImageViewer.bash 1
-../../ImageViewer/ImageViewer.bash 2
+#アームの制御とカメラ画像出力にBodyIoRTCを使った試行
 
-sleep 5
-
+#RTC名を変数に登録
 s=/localhost/TkSlider0.rtc
 r=/localhost/DoubleArmV7-DoubleArmV7PDControllerIoRTC.rtc
 m=/localhost/TkMonitorSlider0.rtc
@@ -15,23 +9,23 @@ c=/localhost/DoubleArmV7-VisionSensorIoRTC.rtc
 v1=/localhost/ImageViewer1.rtc
 v2=/localhost/ImageViewer2.rtc
 
-input=
-while [ "$input" != "y" ]
-do
-    echo -n "進めていいですか？"
-    read input
-done
+./TkSlider.py &
+./TkMonitorSlider.py &
+#../../ImageViewerがあることが前提
+../../ImageViewer/ImageViewer.bash 1
+../../ImageViewer/ImageViewer.bash 2
+
+sleep 3
 
 rtconf $v1 set BGR 1
 rtconf $v2 set BGR 1
 
-rtcon $s:slider $r:qt
-rtcon $r:q $m:value
-
-rtcon $c:FRAME_FRONT_CAMERA $v1:Image
-rtcon $c:WORK_RIGHT_VIEW $v2:Image
-
+#外部のRTCはChoreonoidからactivateされないので
 rtact $s $m $v1 $v2
+
+(cd ..; bin/choreonoid sample/OpenRTM/OpenRTM-DoubleArmV7S-camera.cnoid &)
+
+sleep 3
 
 echo "Chorenoidでシミュレーションを開始，停止してください"
 
@@ -42,6 +36,7 @@ do
     read input
 done
 
+#外部のRTCはChoreonoidからdeactivateされないので
 rtdeact $s $m $v1 $v2
 
 rtexit $s
--- odens/TestDoubleArmV7S-camera2.sh
+++ odens/TestDoubleArmV7S-camera2.sh
@@ -1,38 +1,32 @@
 #!/bin/bash
-(cd ..; bin/choreonoid sample/OpenRTM/OpenRTM-DoubleArmV7S-camera2.cnoid &)
-./TkSlider.py &
-./TkMonitorSlider.py &
-#../../ImageViewerがあることが前提
-../../ImageViewer/ImageViewer.bash 1
-#../../ImageViewer/ImageViewer.bash 2
+#アームの制御にBodyIoRTC，カメラ画像出力にBodyRTC（旧方式）を使った試行
 
-sleep 5
-
+#RTC名を変数に登録
 s=/localhost/TkSlider0.rtc
 r=/localhost/DoubleArmV7-DoubleArmV7PDControllerIoRTC.rtc
 m=/localhost/TkMonitorSlider0.rtc
 c=/localhost/DoubleArmV7.rtc
 v1=/localhost/ImageViewer1.rtc
-v2=/localhost/ImageViewer2.rtc
+p1=/localhost/PointCloudViewer1.rtc
 
-#input=
-#while [ "$input" != "y" ]
-#do
-#    echo -n "進めていいですか？"
-#    read input
-#done
+./TkSlider.py &
+./TkMonitorSlider.py &
+#../../ImageViewerがあることが前提
+../../ImageViewer/ImageViewer.bash 1
+#../../PointCloudViewerがあることが前提
+../../PointCloudViewer/PointCloudViewer.bash 1
+
+sleep 3
 
 rtconf $v1 set BGR 0
-#rtconf $v2 set BGR 1
 
-rtcon $s:slider $r:qt
-rtcon $r:q $m:value
+#PythonのRTCはChoreonoidからactivateされないので
+rtact $s $m
 
-rtcon $c:FRAME_FRONT_CAMERA $v1:Image
-#rtcon $c:WORK_RIGHT_VIEW $v2:Image
+(cd ..; bin/choreonoid sample/OpenRTM/OpenRTM-DoubleArmV7S-camera2.cnoid &)
 
-rtact $s $m $v1 #$v2
-
+sleep 3
+sleep 3
 echo "Chorenoidでシミュレーションを開始，停止してください"
 
 input=
@@ -42,10 +36,11 @@ do
     read input
 done
 
-rtdeact $s $m $v1 #$v2
+#PythonのRTCはChoreonoidからdeactivateされないので
+rtdeact $s $m
 
 rtexit $s
 rtexit $m
 rtexit $v1
-#rtexit $v2
+rtexit $p1
 killall choreonoid
