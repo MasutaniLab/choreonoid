282b0d6de99d31485fd6ec4d82e4903b8a8bfd9d
MASUTANI Yasuhiro <masutani@osakac.ac.jp>
2018-09-01 11:36:32 +0900

    AGXに対応した起動スクリプトを追加

---
+++ odens/TestDoubleArmV7A-camera.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#アームの制御とカメラ画像出力にBodyIoRTCを使った試行
+
+#RTC名を変数に登録
+s=/localhost/TkSlider0.rtc
+r=/localhost/DoubleArmV7-DoubleArmV7PDControllerIoRTC.rtc
+m=/localhost/TkMonitorSlider0.rtc
+c=/localhost/DoubleArmV7-VisionSensorIoRTC.rtc
+v1=/localhost/ImageViewer1.rtc
+v2=/localhost/ImageViewer2.rtc
+p1=/localhost/PointCloudViewer1.rtc
+
+./TkSlider.py &
+./TkMonitorSlider.py &
+#../../ImageViewerがあることが前提
+../../ImageViewer/ImageViewer.bash 1
+../../ImageViewer/ImageViewer.bash 2
+#../../PointCloudViewerがあることが前提
+../../PointCloudViewer/PointCloudViewer.bash 1
+
+sleep 3
+
+#外部のRTCはChoreonoidからactivateされないので
+rtact $s $m $v1 $v2 $p1
+
+(cd ..; bin/choreonoid sample/OpenRTM/OpenRTM-DoubleArmV7A-camera.cnoid &)
+
+sleep 3
+
+echo "Chorenoidでシミュレーションを開始，停止してください"
+
+input=
+while [ "$input" != "y" ]
+do
+    echo -n "終了しますか？"
+    read input
+done
+
+#外部のRTCはChoreonoidからdeactivateされないので
+rtdeact $s $m $v1 $v2  $p1
+
+rtexit $s
+rtexit $m
+rtexit $v1
+rtexit $v2
+rtexit $p1
+killall choreonoid
---
+++ odens/TestDoubleArmV7A.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#アームの制御にBodyIoRTCを使った試行
+
+#RTC名を変数に登録
+s=/localhost/TkSlider0.rtc
+r=/localhost/DoubleArmV7-DoubleArmV7PDControllerIoRTC.rtc
+m=/localhost/TkMonitorSlider0.rtc
+
+./TkSlider.py &
+./TkMonitorSlider.py &
+
+sleep 3
+
+#外部のRTCはChoreonoidからactivateされないので
+rtact $s $m
+
+(cd ..; bin/choreonoid sample/OpenRTM/OpenRTM-DoubleArmV7A-single.cnoid &)
+
+sleep 3
+
+echo "Chorenoidでシミュレーションを開始，停止してください"
+
+input=
+while [ "$input" != "y" ]
+do
+    echo -n "終了しますか？"
+    read input
+done
+
+#外部のRTCはChoreonoidからdeactivateされないので
+rtdeact $s $m
+
+rtexit $s
+rtexit $m
+killall choreonoid
