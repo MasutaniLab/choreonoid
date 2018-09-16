2ba1f93dbaacca5d59d670a97845b63c5ae90241
MASUTANI Yasuhiro <masutani@osakac.ac.jp>
2018-09-16 19:31:20 +0900

    T?-DoubleArmV7-odens.pyにおいて対象センサを指定しないよう変更．VisionSensorIoRTCの出力を限定しないため．

--- sample/WRS2018/T1-DoubleArmV7A-odens.py
+++ sample/WRS2018/T1-DoubleArmV7A-odens.py
@@ -1,4 +1,4 @@
 import WRSUtil
 WRSUtil.loadProject(
     "SingleSceneView", "T1", "AGXSimulator", "DoubleArmV7A-odens",
-    enableVisionSimulation = True, targetVisionSensors = "FRAME_FRONT_CAMERA")
+    enableVisionSimulation = True, targetVisionSensors = "")
--- sample/WRS2018/T6-DoubleArmV7A-odens.py
+++ sample/WRS2018/T6-DoubleArmV7A-odens.py
@@ -1,4 +1,4 @@
 import WRSUtil
 WRSUtil.loadProject(
     "SingleSceneView", "T6", "AGXSimulator", "DoubleArmV7A-odens",
-    enableVisionSimulation = True, targetVisionSensors = "FRAME_FRONT_CAMERA")
+    enableVisionSimulation = True, targetVisionSensors = "")
